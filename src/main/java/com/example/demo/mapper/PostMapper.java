afeccb44a1934d162a5ce77af412579f86343b98
leejihyeon114 <leejihyeon114@gmail.com>
2025-05-14 17:26:08 +0900

    general board

---
+++ src/main/java/com/example/demo/mapper/PostMapper.java
@@ -0,0 +1,26 @@
+package com.example.demo.mapper;
+
+import com.example.demo.domain.post.model.Post;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PostMapper {
+
+    //게시글 작성
+    void insertPost(Post post);
+
+    //게시글 전체조회
+    List<Post> selectAllPost();
+
+    //게시글 단일조회
+    Post selectPostById(int postId);
+
+    //게시글 수정
+    void updatePost(Post post);
+
+    //게시글 삭제
+    void deletePostById(int postId);
+
+}
