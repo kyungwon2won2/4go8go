7edffb5026426dfedf7f853dafe921d155c9b9b5
K-IMjihun <kjh950901@gmail.com>
2025-05-15 15:16:41 +0900

    refactor : 쿠폰 뷰 이미지 구현중

--- src/main/java/com/example/demo/domain/coupon/service/CouponService.java
+++ src/main/java/com/example/demo/domain/coupon/service/CouponService.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 public class CouponService {
 
     public BirthdayCoupon createBirthdayCoupon(Users user) {
-        // UUID를 사용하여 고유한 쿠폰 코드 생성
         String couponCode = UUID.randomUUID().toString().substring(0, 10);
         
         // 현재 날짜 기준으로 쿠폰 유효기간 설정 (7일)
@@ -22,8 +21,8 @@ public class CouponService {
         calendar.add(Calendar.DAY_OF_MONTH, 7);
         Date expiryDate = calendar.getTime();
         
-        // 할인 금액은 랜덤으로 설정 (1000 ~ 10000 사이)
-        int discountAmount = 1000 + (int)(Math.random() * 9000);
+        // 할인 금액
+        int discountAmount = 3000;
         
         // 생일 쿠폰 생성
         return BirthdayCoupon.builder()
