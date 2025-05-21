093173886b8aa21321944735542ab5da8f1f3071
K-IMjihun <kjh950901@gmail.com>
2025-05-21 16:14:31 +0900

    feat : 이메일 검증기능 및 예외가 동적으로 표시되도록 수정

--- src/main/java/com/example/demo/common/email/service/EmailService.java
+++ src/main/java/com/example/demo/common/email/service/EmailService.java
@@ -44,17 +44,22 @@ public class EmailService {
             throw new CustomException(ErrorCode.DUPLICATE_EMAIL_EXIST);
         }
 
+        // 1. 기존 인증 요청 삭제
+        emailVerificationMapper.deleteByEmail(normalizedEmail);
+
+        // 2. 새 인증 코드 생성 및 저장
         String code = generateRandomCode();
+        LocalDateTime expiresAt = LocalDateTime.now().plusMinutes(3);
 
-        EmailVerification emailVerification = new EmailVerification(
-                normalizedEmail,
-                code,
-                LocalDateTime.now().plusMinutes(3)
-        );
+        EmailVerification verification = new EmailVerification(normalizedEmail, code, expiresAt);
+        emailVerificationMapper.insert(verification);
 
-        emailVerificationMapper.insert(emailVerification);
-        sendVerificationEmail(normalizedEmail, code);
+        // 3. 이메일 전송
+//        sendVerificationEmail(normalizedEmail, code);
+
+        log.info(">>>>  code : code");
     }
+
 
     // 이메일 코드 검증(코드 사용 후 삭제, 시도 횟수 제한, 타이밍 공격 대비, 이메일 대소문자 무시)
     @Transactional
@@ -67,20 +72,30 @@ public class EmailService {
             throw new CustomException(ErrorCode.EMAIL_VERIFICATION_NOT_FOUND);
         }
 
+        // 제한시간 만료 시
+        if (verification.isExpired()) {
+            verification.markUsed();
+            emailVerificationMapper.update(verification);
+            throw new CustomException(ErrorCode.EMAIL_VERIFICATION_EXPIRED_OR_LIMITED);
+        }
+
+        // 시도횟수 초과 시
         if (!verification.canAttempt(MAX_TRIES)) {
             throw new CustomException(ErrorCode.EMAIL_VERIFICATION_EXPIRED_OR_LIMITED);
         }
 
-        verification.incrementAttempts();
-
+        // 인증번호 검증
         boolean matched = MessageDigest.isEqual(
                 verification.getCode().getBytes(StandardCharsets.UTF_8),
                 code.getBytes(StandardCharsets.UTF_8)
         );
 
+
+        // 인증번호가 일치하지 않을 시
         if (!matched) {
-            emailVerificationMapper.update(verification);
-            throw new CustomException(ErrorCode.EMAIL_VERIFICATION_CODE_MISMATCH);
+            verification.incrementAttempts();
+//            updateVerificationAttempts(verification); // 별도 트랜잭션 → 커밋됨
+            throw new CustomException(ErrorCode.EMAIL_VERIFICATION_CODE_MISMATCH); // 이후 롤백돼도 영향 없음
         }
 
         verification.markUsed();
@@ -88,6 +103,11 @@ public class EmailService {
 
         return ResponseEntity.ok(new EmailResponse("이메일 인증이 성공했습니다.", true));
     }
+
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    public void updateVerificationAttempts(EmailVerification verification) {
+//        emailVerificationMapper.update(verification);
+//    }
 
 
 
