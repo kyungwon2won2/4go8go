59626452dff66f05b9c3feffd029122889af7a4f
K-IMjihun <kjh950901@gmail.com>
2025-05-22 14:19:13 +0900

    feat : 물품 상세조회 구현중

--- src/main/java/com/example/demo/domain/post/DTO/ProductDetailDto.java
+++ src/main/java/com/example/demo/domain/post/DTO/ProductDetailDto.java
@@ -2,18 +2,31 @@ package com.example.demo.domain.post.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 @Data
 public class ProductDetailDto {
+    // Post
     private int postId;
     private String title;
-    private String content;
-    private String category;          // ex: "전자기기", "의류" 등
-    private int price;
-    private String condition;         // ex: "NEW", "USED" 등
+    private int viewCount;
+    private int commentCount;
     private Date createdAt;
-    private String location;          // 유저 주소
-    private List<String> imageUrls;   // 이미지 경로 리스트
+    private String content;
+
+    // Product
+    private String categoryName;
+    private int price;
+    private String tradeStatus;
+    private String condition;
+
+    // User (seller)
+    private String nickname;
+    private String address;
+    private BigDecimal rating;
+
+    // Image URLs
+    private List<String> imageUrls;
 }
